43fc344c0b92e06f126ce2af6271d2dea1fb5a2c
Saurabh Sood <saurabhsood91@gmail.com>
2017-08-07 22:40:01 -0700

    initial commit

---
+++ app/app.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Main = require('Main');
+
+// Use ES6 destructuring to import stuff needed for React Routing
+// var Route = require('react-router').Route // Otherwise
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+// Load foundation
+// use css loader so that it can be required
+// chain style loader as well
+require('style!css!foundation-sites/dist/foundation.min.css');
+
+// Load our own styles
+require('style!css!sass!applicationStyles');
+
+$(document).foundation();
+
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={Main}>
+        </Route>
+    </Router>,
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        // load jquery, app.jsx and foundation
+        // order matters here
+        // script! needed as jquery and foundation are not webpack suited
+        // using the script-loader module here
+        'script!jquery/dist/jquery.min.js',
+        'script!foundation-sites/dist/foundation.min.js',
+        './app/app.jsx'
+    ],
+    externals: {
+        jquery: 'jQuery' //so that foundation can attach it's method
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$': 'jquery', //Whenever we encounter $, replace with 'jquery' module,
+            'jQuery': 'jquery'
+        })
+    ],
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        root: __dirname,
+        alias: {
+            // Put aliases for components here
+            Main: 'app/components/Main.jsx',
+            Nav: 'app/components/Nav.jsx',
+            applicationStyles: 'app/styles/app.scss'
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map'
+};
