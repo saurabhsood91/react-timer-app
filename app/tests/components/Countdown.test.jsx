13ef5293ae0efcb04b66039d080d9162b2d001aa
Saurabh Sood <saurabhsood91@gmail.com>
2017-08-13 12:25:18 -0700

    more tests

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,44 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jquery');
+var TestUtils = require('react-addons-test-utils');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+    it('should exist', () => {
+        expect(Countdown).toExist();
+    });
+
+    describe('handleSetCountdown', () => {
+        it('should set state to started and count down', (done) => {
+            // render the component
+            var countdown = TestUtils.renderIntoDocument(<Countdown />);
+            countdown.handleSetCountdown(10);
+
+            // count should have been updated
+            // countdown status should have been updated
+            expect(countdown.state.count).toBe(10);
+            expect(countdown.state.countdownStatus).toBe('started');
+
+            // After 1 second, count should be 9
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(9);
+                done();
+            }, 1000);
+        });
+
+        it('should never count down below zero', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown />);
+            countdown.handleSetCountdown(2);
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                done();
+            }, 3000);
+        });
+
+    });
+
+});
